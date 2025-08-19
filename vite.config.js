9242f76e16e7e402a0ae901a6666d25a5bef3314
Karthik Kathari <karthikkathari74@gmail.com>
2025-08-19 20:27:53 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/pokemon-explorer/', // ✅ this is required for GitHub Pages
+  base: '/',
   plugins: [react()],
 })
